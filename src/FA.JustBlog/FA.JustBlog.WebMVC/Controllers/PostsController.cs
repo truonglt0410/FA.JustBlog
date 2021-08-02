43eee388ab2ff0d60a3e85424541e086128c7383
Truonglt4 <nhimshinobi@gmail.com>
2021-08-03 02:09:51 +0700

    update postcontroller lastestpost

--- src/FA.JustBlog/FA.JustBlog.WebMVC/Controllers/PostsController.cs
+++ src/FA.JustBlog/FA.JustBlog.WebMVC/Controllers/PostsController.cs
@@ -13,18 +13,34 @@ namespace FA.JustBlog.WebMVC.Controllers
     public class PostsController : Controller
     {
         private readonly IPostService _postService;
+        private readonly ICategoryService _categoryService;
 
         //private readonly PostsController : Controller;
-        public PostsController(IPostService postService)
+        public PostsController(IPostService postService, ICategoryService categoryService)
         {
             _postService = postService;
+            _categoryService = categoryService;
         }
         // GET: Posts
-        public async  Task<ActionResult> Index()
+        public async  Task<ActionResult> Index(int? pageIndex = 1, int? pageSize = 3)
         {
-           
-            var posts = await _postService.GetAllAsync();
+            Expression<Func<Post, bool>> filter = null;
+
+            Func<IQueryable<Post>, IOrderedQueryable<Post>> orderBy = o => o.OrderBy(p => p.Title);
+            var posts = await _postService.GetAsync(filter: filter, orderBy: orderBy,
+                pageIndex: pageIndex ?? 1, pageSize: pageSize ?? 3);
             return View(posts);
+        }
+        public async Task<ActionResult> LastestPosts()
+        {
+            var lastestPosts = await _postService.GetLatestPostAsync(3);
+            return PartialView(lastestPosts);
+        }
+
+        public async Task<ActionResult> Detail(Guid id)
+        {
+            var post = await _postService.GetByIdAsync(id);
+            return View(post);
         }
     }
 }
